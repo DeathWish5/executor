a2d02ee9567973209f557db53adc988dad2095e5
Runji Wang <wangrunji0408@163.com>
2020-06-23 23:37:09 +0800

    replace infinite loop to run_until_idle. remove arch specific code.

--- executor/src/lib.rs
+++ executor/src/lib.rs
@@ -14,16 +14,9 @@ use {
 };
 
 /// Executor holds a list of tasks to be processed
+#[derive(Default)]
 pub struct Executor {
     tasks: VecDeque<Arc<Task>>,
-}
-
-impl Default for Executor {
-    fn default() -> Self {
-        Executor {
-            tasks: VecDeque::new(),
-        }
-    }
 }
 
 /// Task is our unit of execution and holds a future are waiting on
@@ -45,7 +38,7 @@ impl Task {
         *value = true;
     }
 
-    pub fn sleeping(&self) -> bool {
+    pub fn is_sleeping(&self) -> bool {
         let value = self.state.lock();
         !(*value)
     }
@@ -58,10 +51,10 @@ impl Task {
 
 impl Executor {
     /// Add task for a future to the list of tasks
-    fn add_task(&mut self, future: Box<dyn Future<Output = ()> + 'static + Send + Unpin>) {
+    fn add_task(&mut self, future: Pin<Box<dyn Future<Output = ()> + 'static + Send>>) {
         // store our task
         let task = Arc::new(Task {
-            future: Mutex::new(Box::pin(future)),
+            future: Mutex::new(future),
             state: Mutex::new(true),
         });
         self.tasks.push_back(task);
@@ -71,73 +64,38 @@ impl Executor {
         self.tasks.push_back(task);
     }
 
-    pub fn pop_tasks(&mut self) -> Option<Arc<Task>> {
-        let length = self.tasks.len();
-        let mut res = None;
-        for _ in 0..length {
+    pub fn pop_runnable_task(&mut self) -> Option<Arc<Task>> {
+        for _ in 0..self.tasks.len() {
             let task = self.tasks.pop_front().unwrap();
-            if !task.sleeping() {
-                res = Some(task);
-                break;
-            } else {
-                self.tasks.push_back(task);
+            if !task.is_sleeping() {
+                return Some(task);
             }
+            self.tasks.push_back(task);
         }
-        res
+        None
     }
 }
 
 lazy_static! {
-    static ref GLOBAL_EXECUTOR: Mutex<Box<Executor>> = {
-        let m = Executor::default();
-        Mutex::new(Box::new(m))
-    };
+    static ref GLOBAL_EXECUTOR: Mutex<Executor> = Mutex::new(Executor::default());
 }
 
 /// Give future to global executor to be polled and executed.
 pub fn spawn(future: impl Future<Output = ()> + 'static + Send) {
-    GLOBAL_EXECUTOR.lock().add_task(Box::new(Box::pin(future)));
+    GLOBAL_EXECUTOR.lock().add_task(Box::pin(future));
 }
 
-pub fn run() -> ! {
-    loop {
-        let _task = GLOBAL_EXECUTOR.lock().pop_tasks();
-        if _task.is_some() {
-            let task = _task.unwrap();
-            if task.sleeping() {
-                GLOBAL_EXECUTOR.lock().push_task(task);
-            } else {
-                task.mark_sleep();
-                let mut is_pending = false;
-                {
-                    let mut future = task.future.lock();
-                    // make a waker for our task
-                    let waker = waker_ref(&task);
-                    // poll our future and give it a waker
-                    let context = &mut Context::from_waker(&*waker);
-                    if let Poll::Pending = future.as_mut().poll(context) {
-                        is_pending = true;
-                    }
-                }
-                if is_pending {
-                    GLOBAL_EXECUTOR.lock().push_task(task);
-                }
-            }
-        } else {
-            wait_for_interrupt();
+/// Run futures until there is no runnable task.
+pub fn run_until_idle() {
+    while let Some(task) = { || GLOBAL_EXECUTOR.lock().pop_runnable_task() }() {
+        task.mark_sleep();
+        // make a waker for our task
+        let waker = waker_ref(&task);
+        // poll our future and give it a waker
+        let mut context = Context::from_waker(&*waker);
+        let ret = task.future.lock().as_mut().poll(&mut context);
+        if let Poll::Pending = ret {
+            GLOBAL_EXECUTOR.lock().push_task(task);
         }
-    }
-}
-
-#[cfg(target_arch = "x86_64")]
-fn wait_for_interrupt() {
-    x86_64::instructions::interrupts::enable_interrupts_and_hlt();
-    x86_64::instructions::interrupts::disable();
-}
-
-#[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))]
-fn wait_for_interrupt() {
-    unsafe {
-        riscv::asm::wfi();
     }
 }
