6420e2daeb59cd1537f221d0a79b12deaee36e7d
DeathWish5 <zyr_ms@outlook.com>
2021-11-05 22:10:31 +0800

    add block_on: blocks the current thread on the result of a future

--- executor/src/lib.rs
+++ executor/src/lib.rs
@@ -13,6 +13,11 @@ use {
     woke::{waker_ref, Woke},
 };
 
+#[macro_use]
+extern crate log;
+
+use log::*;
+
 /// Executor holds a list of tasks to be processed
 #[derive(Default)]
 pub struct Executor {
@@ -79,6 +84,30 @@ impl Executor {
     // Give future to be polled and executed
     pub fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
         self.add_task(Box::pin(future));
+    }
+
+    /// Spawns a task and blocks the current thread on its result
+    pub fn block_on<T, F>(future: impl Future<Output = T> + 'static + Send, mut wait: F) -> T
+    where
+        F: FnMut(),
+    {
+        struct NullWaker;
+        impl Woke for NullWaker {
+            fn wake_by_ref(_: &Arc<Self>) {
+                // DO NOTHING
+            }
+        }
+        let null_waker = Arc::new(NullWaker);
+        let waker = waker_ref(&null_waker);
+        let mut context = Context::from_waker(&*waker);
+        let mut future = Box::pin(future);
+        loop {
+            warn!("Block run a future.");
+            if let Poll::Ready(n) = future.as_mut().poll(&mut context) {
+                return n;
+            }
+            wait();
+        }
     }
 
     /// Run futures until there is no runnable task.
